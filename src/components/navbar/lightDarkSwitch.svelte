6e9e508e126620557130e34972b80b08098b3edc
qilingzon <qilingzon@qq.com>
2026-01-05 13:29:22 +0800

    优化：主题和壁纸切换改为点击直接切换

--- src/components/navbar/lightDarkSwitch.svelte
+++ src/components/navbar/lightDarkSwitch.svelte
@@ -48,7 +48,7 @@ function hidePanel() {
 
 <!-- z-50 make the panel higher than other float panels -->
 <div class="relative z-50" role="menu" tabindex="-1" onmouseleave={hidePanel}>
-    <button aria-label="Light/Dark/System Mode" role="menuitem" class="relative btn-plain scale-animation rounded-lg h-11 w-11 active:scale-90" id="scheme-switch" onmouseenter={showPanel} onclick={() => { if (window.innerWidth < 1024) { showPanel(); } else { toggleScheme(); } }}>
+    <button aria-label="Light/Dark/System Mode" role="menuitem" class="relative btn-plain scale-animation rounded-lg h-11 w-11 active:scale-90" id="scheme-switch" onmouseenter={showPanel} onclick={toggleScheme}>
         <div class="absolute" class:opacity-0={mode !== LIGHT_MODE}>
             <Icon icon="material-symbols:wb-sunny-outline-rounded" class="text-[1.25rem]"></Icon>
         </div>
